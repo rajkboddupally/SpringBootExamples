b9540428208443e557780b2d0f2cd349136502d9
Raj Kumar Boddupally <rajkboddupally@gmaill.com>
2021-01-22 23:15:21 -0500

    Initial project files

---
+++ SpringBootExamples/src/test/java/com/springboot/examples/SpringBootExamples/SpringBootExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.examples.SpringBootExamples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootExamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
